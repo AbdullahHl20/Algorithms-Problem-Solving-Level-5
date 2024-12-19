2ff7ca848b459798cfe8ad7e93f3a74266a5bef2
Abdullah Alhelal <abdullah.h@alameensoft.com>
2024-12-19 05:42:59 -0800

    add clsDblLimkedList

--- Algorithms-Problem-Solving-Level-5/Algorithms-Problem-Solving-Level-5.cpp
+++ Algorithms-Problem-Solving-Level-5/Algorithms-Problem-Solving-Level-5.cpp
@@ -2,10 +2,11 @@
 //
 
 #include <iostream>
+#include "clsDblLimkedList.h"
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	clsDblLimkedList<int> myclsDblLimkedList;
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
---
+++ Algorithms-Problem-Solving-Level-5/clsDblLimkedList.h
@@ -0,0 +1,23 @@
+#pragma once
+
+template <class T>
+class  clsNode
+{
+public:
+	T value;
+	clsNode* prev;
+	clsNode* next;
+
+
+};
+template <class T>
+class clsDblLimkedList
+{
+	clsNode<T> node;
+public:
+	void insterAtnegining()
+	{
+	}
+
+};
+
